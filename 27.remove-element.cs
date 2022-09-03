394f7805f4d054c25666ded663665fa6a2f8452c
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-03 12:25:42 -0700

    #27

---
+++ 27.remove-element.cs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=27 lang=csharp
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        short n = 0;
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] == val) {
+                nums[i] = 200;
+                n++;
+            }
+        }
+
+        Array.Sort(nums);
+
+        return nums.Length - n;
+    }
+}
+// @lc code=end
