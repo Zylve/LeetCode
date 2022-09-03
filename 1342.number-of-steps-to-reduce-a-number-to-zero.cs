6ae0451f70579405a540749bdd86288cf7389080
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-03 13:43:45 -0700

    #1342

---
+++ 1342.number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1342 lang=csharp
+ *
+ * [1342] Number of Steps to Reduce a Number to Zero
+ */
+
+// @lc code=start
+// public class Solution {
+//     public int NumberOfSteps(int num) {
+//         int i = 0;
+//         while(num != 0) {
+//             if(num % 2 == 0) {
+//                 num /= 2;
+//             } else {
+//                 num--;
+//             }
+//             i++;
+//         }
+
+//         return i;
+//     }
+// }
+// @lc code=end
