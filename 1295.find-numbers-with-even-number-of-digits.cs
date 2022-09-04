3a89b8986a512e61ee45c00b256ba3bdd184136c
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-03 20:07:00 -0700

    #1295

---
+++ 1295.find-numbers-with-even-number-of-digits.cs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1295 lang=csharp
+ *
+ * [1295] Find Numbers with Even Number of Digits
+ */
+
+// @lc code=start
+// public class Solution {
+//     public int FindNumbers(int[] nums) {
+//         short n = 0;
+//         foreach(int i in nums) {
+//             if(Convert.ToString(i).Length % 2 == 0) {
+//                 n++;
+//             }
+//         }
+
+//         return n;
+//     }
+// }
+// @lc code=end
