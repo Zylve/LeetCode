b8cfb8f7ed8a8815448a76179e8bda8b57ad3e0e
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-03 20:01:09 -0700

    #485

---
+++ 485.max-consecutive-ones.cs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=485 lang=csharp
+ *
+ * [485] Max Consecutive Ones
+ */
+
+// @lc code=start
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int n = nums.Length;
+        int count = 0;
+        int max = int.MinValue;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 0) {
+                if(count > max) {
+                    max = count;
+                }
+
+                count = 0;
+            } else {
+                count++;
+            }
+        }
+
+        if(count > max) {
+            max = count;
+        }
+        return max;
+    }
+}
+// @lc code=end
