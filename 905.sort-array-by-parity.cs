7aa136f9397d221879070728d720086053b38f73
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-05 12:47:48 -0700

    wtf why does this work

---
+++ 905.sort-array-by-parity.cs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=905 lang=csharp
+ *
+ * [905] Sort Array By Parity
+ */
+
+// @lc code=start
+public class Solution {
+    public int[] SortArrayByParity(int[] nums) {
+        int wp = 0;
+
+        for(int i = 0; i < nums.Length; i++) {
+            if(nums[i] % 2 == 0) {
+                (nums[wp], nums[i]) = (nums[i], nums[wp]);
+                wp++;
+            }
+        }
+
+        return nums;
+    }
+}
+// @lc code=end
