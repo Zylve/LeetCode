c51f4e04d8b936eae3fa388575e17b6a6db974bd
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-05 12:30:56 -0700

    #283

--- 283.move-zeroes.cs
+++ 283.move-zeroes.cs
@@ -5,16 +5,16 @@
  */
 
 // @lc code=start
-public class Solution {
-    public void MoveZeroes(int[] nums) {
-        int wP = 0;
+// public class Solution {
+//     public void MoveZeroes(int[] nums) {
+//         int wP = 0;
 
-        for(int i = 0; i < nums.Length; i++) {
-            if(nums[i] != 0) {
-                (nums[wP], nums[i]) = (nums[i], nums[wP]);
-                wP++;
-            }
-        }
-    }
-}
+//         for(int i = 0; i < nums.Length; i++) {
+//             if(nums[i] != 0) {
+//                 (nums[wP], nums[i]) = (nums[i], nums[wP]);
+//                 wP++;
+//             }
+//         }
+//     }
+// }
 // @lc code=end
