b02527c57cb72a264ef58324d702b4bd190b42c4
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-05 17:47:34 -0700

    comment out

--- 414.third-maximum-number.cs
+++ 414.third-maximum-number.cs
@@ -5,28 +5,28 @@
  */
 
 // @lc code=start
-public class Solution {
-    public int ThirdMax(int[] nums) {
-        Array.Sort(nums);
-        int i = nums[0];
-        int j = nums[0];
-        int k = nums[0];
+// public class Solution {
+//     public int ThirdMax(int[] nums) {
+//         Array.Sort(nums);
+//         int i = nums[0];
+//         int j = nums[0];
+//         int k = nums[0];
 
-        HashSet<int> set = new HashSet<int>(nums);
+//         HashSet<int> set = new HashSet<int>(nums);
 
-        if(set.Count < 3) {
-            return nums.Max();
-        }
+//         if(set.Count < 3) {
+//             return nums.Max();
+//         }
 
-        foreach(int l in set) {
-            if(l > i) {
-                k = j;
-                j = i;
-                i = l;
-            }
-        }
+//         foreach(int l in set) {
+//             if(l > i) {
+//                 k = j;
+//                 j = i;
+//                 i = l;
+//             }
+//         }
 
-        return k;
-    }
-}
+//         return k;
+//     }
+// }
 // @lc code=end
