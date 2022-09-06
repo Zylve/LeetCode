f5cbd4161474158320c4873686a548324615c7b0
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-05 17:47:09 -0700

    #414

---
+++ 414.third-maximum-number.cs
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=414 lang=csharp
+ *
+ * [414] Third Maximum Number
+ */
+
+// @lc code=start
+public class Solution {
+    public int ThirdMax(int[] nums) {
+        Array.Sort(nums);
+        int i = nums[0];
+        int j = nums[0];
+        int k = nums[0];
+
+        HashSet<int> set = new HashSet<int>(nums);
+
+        if(set.Count < 3) {
+            return nums.Max();
+        }
+
+        foreach(int l in set) {
+            if(l > i) {
+                k = j;
+                j = i;
+                i = l;
+            }
+        }
+
+        return k;
+    }
+}
+// @lc code=end
