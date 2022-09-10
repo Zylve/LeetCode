d0f62fff57911d3e998951cb85dad471dc0f2f00
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 20:23:31 -0700

    #771

---
+++ 771.jewels-and-stones.cs
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=771 lang=csharp
+ *
+ * [771] Jewels and Stones
+ */
+
+// @lc code=start
+// public class Solution {
+//     public int NumJewelsInStones(string jewels, string stones) {
+//         int x = 0;
+//         foreach(char c in stones) {
+//             if(jewels.Contains(c)) {
+//                 x++;
+//             }
+//         }
+
+//         return x;
+//     }
+// }
+// @lc code=end
