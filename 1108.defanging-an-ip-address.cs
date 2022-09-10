43e8ff67c302146df011ca7b051b834dcb819faf
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 19:42:59 -0700

    #1108

---
+++ 1108.defanging-an-ip-address.cs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1108 lang=csharp
+ *
+ * [1108] Defanging an IP Address
+ */
+
+// @lc code=start
+// public class Solution {
+//     public string DefangIPaddr(string address) {
+//         string defanged = "";
+//         foreach(char c in address) {
+//             if(c == '.') {
+//                 defanged += "[.]";
+//             } else {
+//                 defanged += c;
+//             }
+//         }
+
+//         return defanged;
+//     }
+// }
+// @lc code=end
