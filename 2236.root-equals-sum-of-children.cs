ebf1cb77cf850ed8137a948fbcdef6db1ded8d49
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 20:20:18 -0700

    #2236

---
+++ 2236.root-equals-sum-of-children.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2236 lang=csharp
+ *
+ * [2236] Root Equals Sum of Children
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// public class Solution {
+//     public bool CheckTree(TreeNode root) {
+//         return root.left.val + root.right.val == root.val;
+//     }
+// }
+// @lc code=end
