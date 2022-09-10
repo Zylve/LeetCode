df02840bd686a4567457453d61df9ea6ed3328de
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 19:53:55 -0700

    #2011

---
+++ 2011.final-value-of-variable-after-performing-operations.cs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=2011 lang=csharp
+ *
+ * [2011] Final Value of Variable After Performing Operations
+ */
+
+// @lc code=start
+// public class Solution {
+//     public int FinalValueAfterOperations(string[] operations) {
+//         int x = 0;
+//         foreach(string s in operations) {
+//             if(s.Contains('+')) {
+//                 x++;
+//             } else {
+//                 x--;
+//             }
+//         }
+
+//         return x;
+//     }
+// }
+// @lc code=end
