b572c706cf644edc624754479a8477f1ba5aab15
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 19:34:28 -0700

    tf does this exist

---
+++ 2235.add-two-integers.cs
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=2235 lang=csharp
+ *
+ * [2235] Add Two Integers
+ */
+
+// @lc code=start
+public class Solution {
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+// @lc code=end
