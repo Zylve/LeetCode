0bd5f35a18114e71a44d9c4927ace5c57c4ea350
Zylve <70604613+Zylve@users.noreply.github.com>
2022-09-09 19:34:41 -0700

    tf does this exist

--- 2235.add-two-integers.cs
+++ 2235.add-two-integers.cs
@@ -5,9 +5,9 @@
  */
 
 // @lc code=start
-public class Solution {
-    public int Sum(int num1, int num2) {
-        return num1 + num2;
-    }
-}
+// public class Solution {
+//     public int Sum(int num1, int num2) {
+//         return num1 + num2;
+//     }
+// }
 // @lc code=end
